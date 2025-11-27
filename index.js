b7248ab6f3353579cee99e603ede1599e763fb54
Camilo Delgadillo <devs@neurolynx.tech>
2025-11-27 11:54:23 -0500

    I have built the custom code for creating a record from the form in pug's file. I am working now on styles for this

--- index.js
+++ index.js
@@ -8,81 +8,76 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
-app.get("/")
+// Homepage
+app.get('/', (req, res) => {
+    res.render('homepage', { title: 'Home Page.' });
+});
+
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
-app.get("/update-cobj")
+// * Shows the form
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { 
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum.' 
+    });
+});
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-app.post("/update-cobj")
 
-// * Shows the form
-app.get('/update-cobj', (req, res) => {
-    res.render('updates', 
-        { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum.' });
-});
+// Process form submission
+app.post('/update-cobj', async (req, res) => {
 
-// Process the form
-app.post('/update-cobj', (req, res) => {
-  // Here I will add code to create or update custom object data using the HubSpot API.
-
-  // After processing, redirect to homepage
-  res.redirect('/');
-});
+    // console.log("BODY:", req.body);
+    // console.log("NAME:", req.body.name);
 
 
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
+    const body = {
+        associations: [
+            {
+                to: { id: "177531363999" },
+                types: [
+                    {
+                        associationCategory: "USER_DEFINED",
+                        associationTypeId: 17
+                    }
+                ]
+            }
+        ],
         properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+            name: req.body.name,
+            type_of_game: req.body.type_of_game,
+            gaming_platforms: req.body.gaming_platforms,
+            is_it_cross_platforms_: req.body.is_it_cross_platforms_,
+            hubspot_owner_id: "68530121"
+        },
+        labels: {}
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    const url = "https://api.hubapi.com/crm/v3/objects/2-53428859";
+
+    try {
+        const response = await axios.post(url, body, {
+            headers: {
+                Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+                "Content-Type": "application/json"
+            }
+        });
+        console.log("RESPONSE HS:", response.data);
+
+    } catch (err) {
+        console.error("ERROR:", err.response?.data || err.message);
     }
 
+    res.redirect('/');
 });
-*/
 
 
-// * Localhost
+// Start server
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
